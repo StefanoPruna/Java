21a5c225e908c2427e89bfad54ae20f2d500b32b
StefanoPruna <stefano.pruna@googlemail.com>
2022-03-24 20:23:45 +1100

    Remove unneccessary items

--- 9465_Stefano_Tut7/src/BusinessLogic/Calculate.java
+++ 9465_Stefano_Tut7/src/BusinessLogic/Calculate.java
@@ -5,37 +5,10 @@ import java.util.ArrayList;
 import java.util.List;
 import javafx.scene.control.Label;
 
-public class Calculate implements Calculator
+public class Calculate 
 {  
     public Float result = 0f; 
-    public String operator;
-    public Float number;
-    public Float test;
-
-    @Override
-    public Float sumCalculation() 
-    {
-        return number + number;
-    }
-
-    @Override
-    public Float subtractCalculation() 
-    {
-        return number - number;
-    }
-
-    @Override
-    public Float multiplyCalculation() 
-    {
-        return number / number;
-    }
-
-    @Override
-    public Float divideCalculation() 
-    {
-        return number / number;
-    }
-    
+//    public Float number;
 }
 
 
